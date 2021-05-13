06b39b25faad0a201a23cb0506e8f0af959ec7c8
stacyplot <stacyplot@gmail.com>
2021-05-13 18:46:24 +0300

    create: added reviews section

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,31 +1,28 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+"use strict";
+
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
-
   // Navigation arrows
   navigation: {
     nextEl: '.hotel-slider__button_next',
-    prevEl: '.hotel-slider__button_prev',
+    prevEl: '.hotel-slider__button_prev'
   },
-
   keyboard: {
     enabled: true,
-    onlyInViewport: false,
-  },
+    onlyInViewport: false
+  }
 });
-
-const reviewsSlider = new Swiper('.reviews-slider', {
+var reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
-
   // Navigation arrows
   navigation: {
     nextEl: '.reviews-slider__button_next',
-    prevEl: '.reviews-slider__button_prev',
+    prevEl: '.reviews-slider__button_prev'
   },
-
   keyboard: {
     enabled: true,
-    onlyInViewport: false,
-  },
+    onlyInViewport: false
+  }
 });
